62e572e9d68e185f40bf4518fa6234aa30ae86b8
evanmacbride <ejmacbride@gmail.com>
2020-10-27 11:53:23 -0400

    add new images, run kmeans for k=1, 2, 5, 10, 20

--- main.py
+++ main.py
@@ -2,16 +2,20 @@ from matplotlib import pyplot as plt
 import numpy as np
 import kmeans
 
-loadpath = "chickens-small.jpg"
-savepath = "chickens-edit.jpg"
-# Type of image is a numpy nparray
-image_orig = plt.imread(loadpath)
+image_names = ["chickens", "shibuya", "winter"]
 
-k = 5
-print("Initializing k=" + str(k) + " centers...")
-centers = kmeans.init_centers(k, image_orig)
-print("Running k means...")
-grid, centers = kmeans.run_kmeans(image_orig, centers)
-print("Writing image...")
-image_edit = kmeans.create_new_image(grid, centers)
-plt.imsave(savepath, image_edit)
+for name in image_names:
+    loadpath = name + ".jpg"
+    print("Processing image " + loadpath + "...")
+    # Type of image is a numpy nparray
+    image_orig = plt.imread(loadpath)
+    ks = [1,2,5,10,20]
+    for k in ks:
+        savepath = name + "-k" + str(k) + ".jpg"
+        print("Initializing k=" + str(k) + " centers...")
+        centers = kmeans.init_centers(k, image_orig)
+        print("Running k means...")
+        grid, centers = kmeans.run_kmeans(image_orig, centers)
+        print("Writing image " + savepath + "...")
+        image_edit = kmeans.create_new_image(grid, centers)
+        plt.imsave(savepath, image_edit)
