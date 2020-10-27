2f4a8945057e46ebf3eb78473373ea56cd09b5f1
evanmacbride <ejmacbride@gmail.com>
2020-10-27 11:28:44 -0400

    add README.md

--- main.py
+++ main.py
@@ -7,8 +7,11 @@ savepath = "chickens-edit.jpg"
 # Type of image is a numpy nparray
 image_orig = plt.imread(loadpath)
 
-k = 2
+k = 5
+print("Initializing k=" + str(k) + " centers...")
 centers = kmeans.init_centers(k, image_orig)
+print("Running k means...")
 grid, centers = kmeans.run_kmeans(image_orig, centers)
+print("Writing image...")
 image_edit = kmeans.create_new_image(grid, centers)
 plt.imsave(savepath, image_edit)
