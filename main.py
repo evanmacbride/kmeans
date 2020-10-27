ecdfb1030762b9e2a684fc38c4e68d4904ea206d
evanmacbride <ejmacbride@gmail.com>
2020-10-26 21:41:31 -0400

    clean up comments

--- main.py
+++ main.py
@@ -9,12 +9,6 @@ image_orig = plt.imread(loadpath)
 
 k = 2
 centers = kmeans.init_centers(k, image_orig)
-print("After init: ")
-print(centers)
-#image_edit, distance_sum = kmeans.update_image(image_orig, centers)
 grid, centers = kmeans.run_kmeans(image_orig, centers)
 image_edit = kmeans.create_new_image(grid, centers)
 plt.imsave(savepath, image_edit)
-
-#print(centers)
-#print(distance_sum)
