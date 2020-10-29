b4a4c56a0d7d2175c07599faa02f022ddee064c1
techercise <mjl1997@verizon.net>
2020-10-29 11:26:43 -0400

    Add image for k=2, result image, and get rid of downsampling in kmeans

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import numpy as np
 import kmeans
 import sys
 import cv2
+import time
 
 def run_all():
     '''
@@ -14,23 +15,17 @@ def run_all():
         print("Processing image " + loadpath + "...")
         # Type of image is a numpy 
         image_orig = plt.imread(loadpath)
-        # scale down the image so it's quicker for computing k-means
-        # here we scale down the image to 50% of its original size using nearest neighbor interpolation
-        # the resizing will not smooth the image, thus we should achieve the same results if running on
-        # the original size image
-        downsampled_image = cv2.resize(image_orig, (0,0), fx=0.5, fy=0.5, interpolation=cv2.INTER_NEAREST)
         ks = [1,2,5,10,20]
         for k in ks:
             savepath = name + "-k" + str(k) + ".jpg"
             print("Initializing k=" + str(k) + " centers...")
-            centers = kmeans.init_centers(k, downsampled_image)
+            centers = kmeans.init_centers(k, image_orig)
             print("Running k means...")
-            grid, centers = kmeans.run_kmeans(downsampled_image, centers)
+            grid, centers = kmeans.run_kmeans(image_orig, centers)
             print("Writing image " + savepath + "...")
             image_edit = kmeans.create_new_image(grid, centers)
             # Upsample the image back to its original size to save it
-            upsampled_image = cv2.resize(image_edit, (0, 0), fx=2, fy=2, interpolation=cv2.INTER_NEAREST)
-            plt.imsave(savepath, upsampled_image)
+            plt.imsave(savepath, image_edit)
 
 def run_specific(filename, k):
     '''
@@ -52,7 +47,9 @@ def run_specific(filename, k):
     plt.imsave(savepath, image_edit)
 
 if (len(sys.argv) == 1):
+    print('Start:', time.time())
     run_all()
+    print('Finish:', time.time())
 elif (len(sys.argv) == 3):
     run_specific(sys.argv[1],int(sys.argv[2]))
 else:
